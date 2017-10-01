3d58ba76ea9ed1e8589a82668cf623128f94705b
abhineet123 <asingh1@ualberta.ca>
2017-10-01 15:26:22 -0600

    minor fixes to make it work;

--- compareFiles.py
+++ compareFiles.py
@@ -1,8 +1,8 @@
 import subprocess
 import filecmp
 import os
-dir_1 = '~/mdp_log'
-dir_2 = '~/mdp_tf_log'
+dir_1 = '/root/mdp_log'
+dir_2 = '/root/mdp_tf_log'
 filenames = [
     ['indices.txt', 'templates/indices.txt'],
     ['overlaps.txt', 'templates/overlaps.txt'],
@@ -19,13 +19,13 @@ for files in filenames:
     path_1  = '{:s}/{:s}'.format(dir_1, files[0])
     path_2 = '{:s}/{:s}'.format(dir_2, files[1])
 
-    if not os.path.exists(path_1):
+    if not os.path.isfile(path_1):
         print '{:s} does not exist'.format(path_1)
         continue
-    if not os.path.exists(path_2):
+    if not os.path.isfile(path_2):
         print '{:s} does not exist'.format(path_2)
         continue
     subprocess.call('dos2unix {:s}'.format(path_1), shell=True)
     subprocess.call('dos2unix {:s}'.format(path_2), shell=True)
     if not filecmp.cmp(path_1, path_2):
-        print 'Files {:s} and {:s} are different'.format(path_1, path_2)
+        print '\nFiles {:s} and {:s} are different\n'.format(path_1, path_2)
