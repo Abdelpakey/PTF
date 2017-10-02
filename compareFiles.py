fd1397e3f64ea39788c27aca67c1c8519f05413b
abhineet123 <asingh1@ualberta.ca>
2017-10-01 20:29:51 -0600

    Updated the data file comparing script to automatically replace the Matlab NaN  with the Numpy nan;

--- compareFiles.py
+++ compareFiles.py
@@ -4,16 +4,18 @@ import os
 dir_1 = '/root/mdp_log'
 dir_2 = '/root/mdp_tf_log'
 filenames = [
-    ['indices.txt', 'templates/indices.txt'],
-    ['overlaps.txt', 'templates/overlaps.txt'],
-    ['ratios.txt', 'templates/ratios.txt'],
-    ['angles.txt', 'templates/angles.txt'],
-    ['bb_overlaps.txt', 'templates/bb_overlaps.txt'],
-    ['similarity.txt', 'templates/similarity.txt'],
-    ['scores.txt', 'templates/scores.txt'],
-    ['roi.txt', 'templates/roi.txt'],
-    ['patterns.txt', 'templates/patterns.txt'],
-
+    ['features.txt', 'templates/features.txt', 0],
+    ['scores.txt', 'templates/scores.txt', 0],
+    ['indices.txt', 'templates/indices.txt', 0],
+    ['overlaps.txt', 'templates/overlaps.txt', 0],
+    ['ratios.txt', 'templates/ratios.txt', 0],
+    ['angles.txt', 'templates/angles.txt', 0],
+    ['bb_overlaps.txt', 'templates/bb_overlaps.txt', 0],
+    ['similarity.txt', 'templates/similarity.txt', 0],
+    ['scores.txt', 'templates/scores.txt', 0],
+    ['roi.txt', 'templates/roi.txt', 0],
+    ['patterns.txt', 'templates/patterns.txt', 0],
+    ['lk_out.txt', 'lkcv/lk_out.txt', 1],
 ]
 for files in filenames:
     path_1  = '{:s}/{:s}'.format(dir_1, files[0])
@@ -25,7 +27,10 @@ for files in filenames:
     if not os.path.isfile(path_2):
         print '{:s} does not exist'.format(path_2)
         continue
-    subprocess.call('dos2unix {:s}'.format(path_1), shell=True)
-    subprocess.call('dos2unix {:s}'.format(path_2), shell=True)
+    subprocess.call('dos2unix -q {:s}'.format(path_1), shell=True)
+    subprocess.call('dos2unix -q {:s}'.format(path_2), shell=True)
+    if files[2]:
+        subprocess.call('sed -i -e \'s/NaN/nan/g\' {:s}'.format(path_1), shell=True)
+	
     if not filecmp.cmp(path_1, path_2):
-        print '\nFiles {:s} and {:s} are different\n'.format(path_1, path_2)
+        print 'Files {:s} and {:s} are different'.format(path_1, path_2)
