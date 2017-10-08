44b1f27e1fc23d4429648f7352de440ccc5ad034
abhineet123 <asingh1@ualberta.ca>
2017-10-08 10:10:47 -0600

    created script to filter lines;

---
+++ filterLines.py
@@ -0,0 +1,28 @@
+import sys
+filter_string = 'magnet'
+in_fname = 'm.txt'
+out_fname = 'filtered.txt'
+arg_id = 1
+if len(sys.argv) > arg_id:
+    filter_string = sys.argv[arg_id]
+    arg_id += 1
+if len(sys.argv) > arg_id:
+    in_fname = sys.argv[arg_id]
+    arg_id += 1
+if len(sys.argv) > arg_id:
+    out_fname = sys.argv[arg_id]
+    arg_id += 1
+
+print 'Filtering lines starting with {:s} in {:s} to {:s}'.format(filter_string, in_fname, out_fname)
+
+out_fid = open(in_fname, 'w')
+lines = open(in_fname, 'r').readlines()
+n_filtered_lines = 0
+for line in lines:
+    if line.startswith(filter_string):
+        out_fid.write(line)
+        n_filtered_lines += 1
+print 'Done filtering {:d} lines'.format(n_filtered_lines)
+out_fid.close()
+
+
