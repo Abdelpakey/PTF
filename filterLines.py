adf98ee57b657338c7b5403d07650d94e5e45e42
abhineet123 <asingh1@ualberta.ca>
2017-10-10 08:44:56 -0600

    Modified line filtering script to search anywhere in the line for the substring;

--- filterLines.py
+++ filterLines.py
@@ -1,5 +1,9 @@
 import sys
+
 filter_string = 'magnet'
+# 0: only at start
+# 1: anywhere
+filter_type = 0
 in_fname = 'm.txt'
 out_fname = 'filtered.txt'
 arg_id = 1
@@ -13,16 +17,22 @@ if len(sys.argv) > arg_id:
     out_fname = sys.argv[arg_id]
     arg_id += 1
 
-print 'Filtering lines starting with {:s} in {:s} to {:s}'.format(filter_string, in_fname, out_fname)
+if filter_type == 0:
+    print 'Filtering lines starting with {:s} in {:s} to {:s}'.format(filter_string, in_fname, out_fname)
+else:
+    print 'Filtering lines containing {:s} in {:s} to {:s}'.format(filter_string, in_fname, out_fname)
 
 out_fid = open(out_fname, 'w')
 lines = open(in_fname, 'r').readlines()
 n_filtered_lines = 0
 for line in lines:
-    if line.startswith(filter_string):
+    retain_line = False
+    if filter_type == 0:
+        retain_line = line.startswith(filter_string)
+    elif filter_type == 1:
+        retain_line = filter_string in line
+    if retain_line:
         out_fid.write(line)
         n_filtered_lines += 1
 print 'Done filtering {:d} lines'.format(n_filtered_lines)
 out_fid.close()
-
-
