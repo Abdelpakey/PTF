0427d40c0ecd973f12ca769f50a191418541ed7a
abhineet123 <asingh1@ualberta.ca>
2017-11-11 23:48:55 -0700

    fixed the n_frames computation in xmlDETRACToMOT;

--- xmlDETRACToMOT.py
+++ xmlDETRACToMOT.py
@@ -1,5 +1,6 @@
 import xml.etree.cElementTree as ET
 from Misc import getParamDict
+import glob
 
 root_dir = 'C:/Datasets'
 actor_id = 4
@@ -18,10 +19,13 @@ for seq_id in xrange(start_id, end_id+1):
     fname = '{:s}/{:s}/Annotations/{:s}.xml'.format(root_dir, actor, seq_name)
     tree = ET.parse(fname)
     root = tree.getroot()
-    n_frames = 0
+
+    img_dir = '{:s}/{:s}/Images/{:s}'.format(root_dir, actor, seq_name)
+    n_frames = len(glob.glob('{:s}/*.jpg'.format(img_dir)))
+    n_frames_list.append(n_frames)
+
     print 'Processing sequence {:d} :: {:s}'.format(seq_id, seq_name)
     for frame_obj in tree.iter('frame'):
-        n_frames += 1
         tsrget_list = frame_obj.find('target_list')
         frame_id = int(frame_obj.attrib['num'])
         for obj in tsrget_list.iter('target'):
@@ -32,9 +36,9 @@ for seq_id in xrange(start_id, end_id+1):
             width = float(bndbox.attrib['width'])
             height = float(bndbox.attrib['height'])
             out_fid.write('{:d},{:d},{:f},{:f},{:f},{:f},1,-1,-1,-1\n'.format(
-                frame_id + 1, obj_id, xmin, ymin, width, height))
-        if (frame_id + 1) % 100 == 0:
-            print '\t Done {:d} frames'.format(frame_id + 1)
-    n_frames_list.append(n_frames)
+                frame_id, obj_id, xmin, ymin, width, height))
+        if frame_id  % 100 == 0:
+            print '\t Done {:d}/{:d} frames'.format(frame_id, n_frames)
+
     out_fid.close()
 print n_frames_list
