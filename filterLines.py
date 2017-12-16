0eec0f94353aefe9d784bf359d333064a38db036
abhineet123 <asingh1@ualberta.ca>
2017-12-15 22:30:04 -0700

    updated filterLines.py to support line retention;

--- filterLines.py
+++ filterLines.py
@@ -5,8 +5,9 @@ filter_string = 'magnet'
 # 0: only at start
 # 1: anywhere
 filter_type = 0
-in_fname = 'm.txt'
+in_fname = 'all.txt'
 out_fname = 'filtered.txt'
+retain_filtered = 0
 arg_id = 1
 if len(sys.argv) > arg_id:
     filter_string = sys.argv[arg_id]
@@ -20,16 +21,22 @@ if len(sys.argv) > arg_id:
 if len(sys.argv) > arg_id:
     out_fname = sys.argv[arg_id]
     arg_id += 1
-
+if len(sys.argv) > arg_id:
+    retain_filtered = int(sys.argv[arg_id])
+    arg_id += 1
+	
 if not os.path.isfile(in_fname):
     print 'Input file {:s} does not exist'.format(in_fname)
     exit(0)
+
 
 if filter_type == 0:
     print 'Filtering lines starting with {:s} in {:s} to {:s}'.format(filter_string, in_fname, out_fname)
 else:
     print 'Filtering lines containing {:s} in {:s} to {:s}'.format(filter_string, in_fname, out_fname)
-
+if retain_filtered:
+    print 'Retaining the filtered lines'
+	
 out_fid = open(out_fname, 'w')
 lines = open(in_fname, 'r').readlines()
 n_filtered_lines = 0
@@ -38,8 +45,10 @@ for line in lines:
     if filter_type == 0:
         retain_line = line.startswith(filter_string)
     elif filter_type == 1:
-        retain_line = filter_string in line
-    if retain_line:
+        retain_line = filter_string in line    
+	if retain_filtered:
+        retain_line = not retain_line
+	if retain_line:
         out_fid.write(line)
         n_filtered_lines += 1
 print 'Done filtering {:d} lines'.format(n_filtered_lines)
