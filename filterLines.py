ec36b8f8433ad6078525fa714c66c4f94fdb6085
abhineet123 <asingh1@ualberta.ca>
2018-02-19 15:16:12 -0700

    2:58 PM Created initial version of the Python script to crop videos; 3:11 PM Got the script working; 3:15 PM added vis_resize_factor;

--- filterLines.py
+++ filterLines.py
@@ -7,7 +7,7 @@ filter_string = 'magnet'
 filter_type = 0
 in_fname = 'all.txt'
 out_fname = 'filtered.txt'
-retain_filtered = 0
+retain_filtered = 1
 arg_id = 1
 if len(sys.argv) > arg_id:
     filter_string = sys.argv[arg_id]
@@ -19,12 +19,13 @@ if len(sys.argv) > arg_id:
     in_fname = sys.argv[arg_id]
     arg_id += 1
 if len(sys.argv) > arg_id:
-    out_fname = sys.argv[arg_id]
-    arg_id += 1
-if len(sys.argv) > arg_id:
     retain_filtered = int(sys.argv[arg_id])
     arg_id += 1
-	
+if len(sys.argv) > arg_id:
+    out_fname = sys.argv[arg_id]
+    arg_id += 1
+
+
 if not os.path.isfile(in_fname):
     print 'Input file {:s} does not exist'.format(in_fname)
     exit(0)
@@ -34,8 +35,8 @@ if filter_type == 0:
     print 'Filtering lines starting with {:s} in {:s} to {:s}'.format(filter_string, in_fname, out_fname)
 else:
     print 'Filtering lines containing {:s} in {:s} to {:s}'.format(filter_string, in_fname, out_fname)
-if retain_filtered:
-    print 'Retaining the filtered lines'
+if not retain_filtered:
+    print 'Removing the filtered lines'
 	
 out_fid = open(out_fname, 'w')
 lines = open(in_fname, 'r').readlines()
@@ -46,9 +47,9 @@ for line in lines:
         retain_line = line.startswith(filter_string)
     elif filter_type == 1:
         retain_line = filter_string in line    
-	if retain_filtered:
+    if not retain_filtered:
         retain_line = not retain_line
-	if retain_line:
+    if retain_line:
         out_fid.write(line)
         n_filtered_lines += 1
 print 'Done filtering {:d} lines'.format(n_filtered_lines)
--- videoToImgSeq.py
+++ videoToImgSeq.py
@@ -5,12 +5,13 @@ import sys
 from Misc import processArguments
 
 params = {
-    'db_root_dir': 'C:/Datasets',
-    'actor': 'GRAM',
-    'seq_name': 'ISL16F8J_TMC_SCU2DJ_2016-10-05_0700',
-    'vid_fmt': 'mp4',
+    'db_root_dir': 'N:\Datasets',
+    'actor': 'ISL',
+    'seq_name': 'DJI_0002',
+    'vid_fmt': 'mov',
     'show_img': 0,
-    'n_frames': 10000
+    'n_frames': 2000,
+    'roi': [1821,231,2373,467]
 }
 
 if __name__ == '__main__':
@@ -22,18 +23,27 @@ if __name__ == '__main__':
     show_img = params['show_img']
     vid_fmt = params['vid_fmt']
     n_frames = params['n_frames']
+    roi = params['roi']
+
+    roi_enabled = False
+
+    if roi is not None:
+        xmin, ymin, xmax, ymax = roi
+        if xmax > xmin and ymax > ymin:
+            print('Using roi: ', roi)
+            roi_enabled = True
 
     print 'actor: ', actor
     print 'seq_name: ', seq_name
 
-    src_fname = db_root_dir + '/' + actor + '/Images/' + seq_name + '.' + vid_fmt
+    src_fname = db_root_dir + '/' + actor + '/' + seq_name + '.' + vid_fmt
 
     dst_dir = db_root_dir + '/' + actor + '/Images/' + seq_name
 
     if not os.path.isdir(dst_dir):
         os.makedirs(dst_dir)
 
-    print 'Reading video from: {:s}'.format(src_fname)
+    print 'Reading video file: {:s}'.format(src_fname)
     cap = cv2.VideoCapture()
     if not cap.open(src_fname):
         raise StandardError('The video file ' + src_fname + ' could not be opened')
@@ -44,6 +54,8 @@ if __name__ == '__main__':
         if not ret:
             print('Frame {:d} could not be read'.format(frame_id + 1))
             break
+        if roi_enabled:
+            frame = frame[roi[1]:roi[3], roi[0]:roi[2], :]
         curr_img = cv2.imwrite(dst_dir + '/image{:06d}.jpg'.format(frame_id + 1), frame)
         if show_img:
             cv2.imshow('Frame', frame)
@@ -52,6 +64,10 @@ if __name__ == '__main__':
         frame_id += 1
         if n_frames > 0 and frame_id >= n_frames:
             break
-        if frame_id % 100 == 0:
-            print 'Done {:d} frames'.format(frame_id)
+        sys.stdout.write('\rDone {:d}/{:d} frames'.format(
+            frame_id + 1, n_frames))
+        sys.stdout.flush()
+    sys.stdout.write('\n')
+    sys.stdout.flush()
+
 
