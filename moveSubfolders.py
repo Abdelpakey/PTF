502e5a36311cdb116ccfb92caab3d2dbfd61b424
abhineet123 <asingh1@ualberta.ca>
2018-06-29 23:53:04 -0600

    minor fix

--- moveSubfolders.py
+++ moveSubfolders.py
@@ -14,13 +14,16 @@ dst_dir = params['dst_dir']
 if not dst_dir:
     dst_dir = src_dir
 
-subfolders = [f for f in glob.iglob(src_dir + '/**/', recursive=True) if os.path.isdir(f) and not 'annotations' in f]
-
+subfolders = [f for f in glob.iglob(src_dir + '/**/', recursive=True) if os.path.isdir(f) and
+              not 'annotations' in f and f != 'bin']
 
 for src in subfolders:
     print('moving {}'.format(src))
     try:
         shutil.move(src, dst_dir)
     except shutil.Error:
+        print('Failure')
+        continue
+    except FileNotFoundError:
         print('Failure')
         continue
