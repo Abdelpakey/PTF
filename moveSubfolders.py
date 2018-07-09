5d6e8c5eff870a7e40e797b78296ae50d3dce6db
abhineet123 <asingh1@ualberta.ca>
2018-07-08 23:52:25 -0600

    minor fix

--- moveSubfolders.py
+++ moveSubfolders.py
@@ -18,7 +18,7 @@ src_dir = os.path.abspath(src_dir)
 dst_dir = os.path.abspath(dst_dir)
 
 subfolders = [os.path.abspath(f) for f in glob.iglob(src_dir + '/**/', recursive=True) if os.path.isdir(f) and
-               'annotations' not in f and os.path.basename(f) != 'bin']
+              'annotations' not in f and os.path.basename(f) != 'bin']
 
 for src in subfolders:
     if src == src_dir or src == dst_dir:
