a10e13c7ab8fec166b5e6e1f01f0053514feec02
abhineet123 <asingh1@ualberta.ca>
2018-07-20 06:47:11 -0600

    minor fix

--- zipMulti.py
+++ zipMulti.py
@@ -4,37 +4,36 @@ from Misc import processArguments, sortKey
 
 if __name__ == '__main__':
     params = {
-        'list_file': '',
-        'file_name': '',
-        'root_dir': '',
+        'dir_names': [],
         'out_name': '',
     }
     processArguments(sys.argv[1:], params)
-    list_file = params['list_file']
-    root_dir = params['root_dir']
-    file_name = params['file_name']
+    dir_names = params['dir_names']
     out_name = params['out_name']
 
-    if list_file:
-        if os.path.isdir(list_file):
-            zip_paths = [os.path.join(list_file, name) for name in os.listdir(list_file) if
-                         os.path.isdir(os.path.join(list_file, name))]
-            zip_paths.sort(key=sortKey)
-        else:
-            zip_paths = [x.strip() for x in open(list_file).readlines() if x.strip()]
-            if root_dir:
-                zip_paths = [os.path.join(root_dir, name) for name in zip_paths]
-    else:
-        zip_paths = [file_name]
+    zip_path = os.path.join(dir_names)
+
+    print('dir_names: ', dir_names)
+    print('zip_path: ', zip_path)
+
+    if not out_name:
+
+        for _dir in dir_names[1:]:
+            out_name = '{}_{}'.format(out_name, _dir) if out_name else _dir
+
+    print('out_name: ', out_name)
 
     zip_cmd = 'zip -r {:s}'.format(out_name)
-    for zip_path in zip_paths:
-        zip_cmd = '{:s} {:s}'.format(zip_cmd, zip_path)
-
+    zip_cmd = '{:s} {:s}'.format(zip_cmd, zip_path)
 
     print('\nrunning: {}\n'.format(zip_cmd))
     # subprocess.call(zip_cmd)
     os.system(zip_cmd)
+
+
+
+
+
 
 
 
